6199cc11de950bd2c1e7d715ffb8dc4d613b24f8
klara8 <97557554+klara8@users.noreply.github.com>
2024-11-05 16:25:25 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,61 +1,28 @@
-import java.sql.SQLSyntaxErrorException;
-import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
+        int delenoTrema = 3;
+        int delenoPeti = 5;
+        int delenoTremaAPeti = 3*5;
 
 
         Scanner scanner = new Scanner(System.in);
-
-        int principal = 0;
-
-    while (true){
-        System.out.print("Principal (1K Kč - 1M Kč): ");
-        principal = scanner.nextInt();
-        if (principal >= 1000 && principal <= 1000000)
-            break;
-        System.out.println("Vlož hodnotu mezi 1 000 a 1 000 000.");
-    }
-
-        float annualInterestRate = 0;
-        float monthlyInterestRate = 0;
+        System.out.print("Number: ");
+        int number = scanner.nextInt();
 
 
-    while (true) {
-        System.out.print("Annual Interest Rate: ");
-        annualInterestRate = scanner.nextFloat();
-        if (annualInterestRate > 0 && annualInterestRate < 30) {
-             monthlyInterestRate = annualInterestRate / MONTHS_IN_YEAR / PERCENT;
-            break;
+        if(number%delenoTremaAPeti == 0){
+            System.out.println("FizzBuzz");
         }
-        System.out.println("Vložte číslo od 0 do 30.");
-    }
-
-
-        int periodInMonths = 0;
-        int period = 0;
-
-    while(true){
-        System.out.print("Period (Years): ");
-        period = scanner.nextInt();
-        if(period > 1 && period < 30){
-            periodInMonths = period * MONTHS_IN_YEAR;
-            break;
+        else if(number%delenoTrema == 0){
+            System.out.println("Buzz");
         }
-        System.out.println("Vložte číslo mezi 1 a 30.");
-    }
+        else if (number%delenoPeti == 0) {
+            System.out.println("Fizz");
+        }
+        else System.out.println(number);
 
-        double mortgage = principal
-                * monthlyInterestRate
-                * Math.pow((1 + monthlyInterestRate), periodInMonths) / (Math.pow((1 + monthlyInterestRate), periodInMonths) - 1);
 
-        String formattedMortgage = NumberFormat.getCurrencyInstance().format(mortgage);
-
-        System.out.println("Mortgage: " + formattedMortgage);
     }
 }
-
-
